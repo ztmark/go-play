bf0cac015e24cd6d022abba03944c71ca795484a
Mark <shuyumark@gmail.com>
2018-01-07 21:12:49 +0800

    eval

--- src/chapter7/http1.go
+++ src/chapter7/http1.go
@@ -14,31 +14,62 @@ func (d dollars) String() string {
     return fmt.Sprintf("$%.2f", d)
 }
 
-func (db database) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-    switch r.URL.Path {
-    case "/list":
-        for item, price := range db {
-            fmt.Fprintf(w, "%s: %s\n", item, price)
-        }
-    case "/price":
-        item := r.URL.Query().Get("item")
-        price, ok := db[item]
-        if !ok {
-            w.WriteHeader(http.StatusNotFound)
-            fmt.Fprintf(w, "no such item: %q\n", item)
-            return
-        }
-        fmt.Fprintf(w, "%s\n", price)
-    default:
-        w.WriteHeader(http.StatusNotFound)
-        fmt.Fprintf(w, "no such page: %s\n", r.URL)
+func (db database) list(w http.ResponseWriter, r *http.Request) {
+    for item, price := range db {
+        fmt.Fprintf(w, "%s: %s\n", item, price)
     }
-
-
 }
+
+func (db database) price(w http.ResponseWriter, r *http.Request) {
+    item := r.URL.Query().Get("item")
+    price, ok := db[item]
+    if !ok {
+        w.WriteHeader(http.StatusNotFound)
+        fmt.Fprintf(w, "no such item: %q\n", item)
+        return
+    }
+    fmt.Fprintf(w, "%s\n", price)
+}
+
+//func (db database) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//
+//    switch r.URL.Path {
+//    case "/list":
+//        for item, price := range db {
+//            fmt.Fprintf(w, "%s: %s\n", item, price)
+//        }
+//    case "/price":
+//        item := r.URL.Query().Get("item")
+//        price, ok := db[item]
+//        if !ok {
+//            w.WriteHeader(http.StatusNotFound)
+//            fmt.Fprintf(w, "no such item: %q\n", item)
+//            return
+//        }
+//        fmt.Fprintf(w, "%s\n", price)
+//    default:
+//        w.WriteHeader(http.StatusNotFound)
+//        fmt.Fprintf(w, "no such page: %s\n", r.URL)
+//    }
+//
+//
+//}
 
 func main() {
     db := database{"shoes": 50, "socks": 5}
-    log.Fatal(http.ListenAndServe("localhost:8080", db))
+
+    mux := http.NewServeMux()
+    mux.Handle("/list", http.HandlerFunc(db.list))
+    mux.Handle("/price", http.HandlerFunc(db.price))
+
+    // use default server mux
+    go func() {
+        http.HandleFunc("/list", db.list)
+        http.HandleFunc("/price", db.price)
+        log.Fatal(http.ListenAndServe("localhost:8680", nil))
+    }()
+
+    log.Fatal(http.ListenAndServe("localhost:8080", mux))
+
+
 }
