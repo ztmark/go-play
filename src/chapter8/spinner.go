ad67db46479db67a348c2e05b20c4560f3e6597c
Mark <shuyumark@gmail.com>
2018-01-08 12:48:52 +0800

    clock1

---
+++ src/chapter8/spinner.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "time"
+    "fmt"
+)
+
+func main() {
+    go spinner(100 * time.Millisecond)
+    const n = 45
+    fibN := fib(n)
+    fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+func fib(x int) int {
+    if x < 2 {
+        return x
+    }
+    return fib(x - 2) + fib(x - 1)
+}
+func spinner(delay time.Duration) {
+    for {
+        for _, r := range `-\|/` {
+            fmt.Printf("\r%c", r)
+            time.Sleep(delay)
+        }
+    }
+}
