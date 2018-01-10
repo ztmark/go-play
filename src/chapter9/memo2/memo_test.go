10ba8358df9525514592dc6585fbfb237d275ebc
Mark <shuyumark@gmail.com>
2018-01-10 21:32:32 +0800

    memo

---
+++ src/chapter9/memo2/memo_test.go
@@ -0,0 +1,22 @@
+package memo_test
+
+import (
+    "chapter9/memotest"
+    "testing"
+    "chapter9/memo2"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+    m := memo.New(httpGetBody)
+    defer m.Close()
+    memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+    m := memo.New(httpGetBody)
+    defer m.Close()
+    memotest.Concurrent(t, m)
+}
+
